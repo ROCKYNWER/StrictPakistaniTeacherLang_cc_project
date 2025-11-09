dc01c5e9032f8b4422e790a5af13db4607900ee0
Shahab Ahmad <95880900+ROCKYNWER@users.noreply.github.com>
2025-11-09 23:12:13 +0500

    Add files via upload

---
+++ test_data.l.ts
@@ -0,0 +1,10 @@
+Khamoshi {
+   Kitne-number marks likh-do 33 bas!
+   Dekhlo marks zyada 50 {
+       Jawab-do "Pass" bas!
+   } Nahintoh {
+       Jawab-do "Fail" bas!
+   }
+   Naam-batao student likh-do "Shahab Ahmad" bas!
+   // chup This is a test
+}
